d606fee74bcee77502be6bf0288d5405988d15a4
chushkin <eugene.chushkin@gmail.com>
2022-05-04 12:51:10 +0300

    [Web] added config

---
+++ src/test/java/helpers/config/WebConfig.java
@@ -0,0 +1,8 @@
+package helpers.config;
+
+import lombok.Data;
+
+@Data
+public class WebConfig {
+    private String baseUrl;
+}
--- src/test/java/web/BaseTest.java
+++ src/test/java/web/BaseTest.java
@@ -2,6 +2,7 @@ package web;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import helpers.config.TestConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,10 +10,11 @@ import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.open;
 
 public abstract class BaseTest {
+    private static final TestConfigFactory config = TestConfigFactory.getInstance();
 
     @BeforeAll
-    public static void setUp(){
-        Configuration.baseUrl = "https://react-redux.realworld.io/";
+    public static void setUp() {
+        Configuration.baseUrl = config.getWebConfig().getBaseUrl();
     }
 
     @BeforeEach
